b66708508dbf4f8d0f9020e09374b0da1be5077f
Olivier Monnier <monnier@indig-net.com>
2023-02-22 18:05:53 +0100

    add script import

--- vue3/index.js
+++ vue3/index.js
@@ -1,6 +1,6 @@
 const libraries = {
   vue: "https://unpkg.com/vue@3/dist/vue.global.js",
-  pinia: "https://unpkg.com/pinia",
+  "pinia:module": "https://unpkg.com/pinia/dist/pinia.iife.js",
   router: "https://unpkg.com/vue-router@4",
   i18n: "https://unpkg.com/vue-i18n@8",
 };
@@ -20,31 +20,34 @@ function createScript(scriptName) {
   })
 }
 
-function handlePromiseResults(results) {
-  const errors = results
-    .filter((result) => result.status === "rejected")
-    .map((result) => (result).reason);
-
-  if (errors.length) {
-    // Aggregate all errors into one
-    throw new AggregateError(errors);
-  }
-
-  return results.map((result) => result.value);
+function createScriptImport() {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    const scripts = Object.entries(libraries).filter(([key, _]) => key.endsWith(':module')).map(([key, value]) => [key.replace(':module', ''), value])
+    script.type = "importmap";
+    script.innerHTML = JSON.stringify({
+      imports: Object.fromEntries(scripts)
+    }, null, "  ")
+    script.onload = function () {
+      return resolve();
+    };
+    script.onerror = function () {
+      return reject("Error occurred while loading script");
+    };
+    document.head.appendChild(script);
+  })
 }
 
-export async function init(scriptNames = []) {
-  const promises = [];
+export async function init(scriptNames = [], callback = null) {
   const list = new Set(['vue', ...scriptNames])
-  for (const scriptName of list) {
-    promises.push(createScript(scriptName))
-  }
-  const results = await Promise.allSettled(promises)
+
   try {
-    handlePromiseResults(results);
-  } catch(err) {
-    for (const error of err.errors) {
-      console.error(error);
+    await createScriptImport()
+    for(const scriptName of list) {
+      await createScript(scriptName)
     }
+    if (typeof callback === "function") callback()
+  } catch(err) {
+    console.error(err)
   }
 }
