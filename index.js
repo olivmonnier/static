18976e03593f1d72372cf53dea7e9126a022d3ac
Olivier Monnier <omonnier.dev@gmail.com>
2023-02-23 22:01:27 +0100

    WIP

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-const ESM_CDN = "https://esm.sh";
-const CJS_CDN = "https://unpkg.com";
-
-function createScript(scriptName) {
-  return new Promise((resolve, reject) => {
-    const script = document.createElement('script');
-    script.src = `${CJS_CDN}/${scriptName}`
-    script.onload = function() {
-      return resolve()
-    }
-    script.onerror = function() {
-      return reject("Error occurred while loading script");
-    }
-    document.body.appendChild(script)
-  })
-}
-
-function createImportMap(libraries) {
-  const importMaps = {}
-  for (let imp of libraries) {
-    importMaps[imp.split('@')[0]] = `${ESM_CDN}/${imp}`
-  }
-  const importMap = {
-    imports: importMaps,
-  };
-  const im = document.createElement("script");
-  im.type = "importmap";
-  im.textContent = JSON.stringify(importMap);
-  document.currentScript.after(im);
-}
-
-async function initialize(scriptNames = []) {
-  const list = new Set([...scriptNames])
-  const eventReady = new CustomEvent('static-ready')
-  createImportMap(list)
-  try {
-    for(const scriptName of list) {
-      await createScript(scriptName)
-    }
-    document.dispatchEvent(eventReady);
-  } catch(err) {
-    console.error(err)
-  }
-}
-
 (async () => {
   let libraries = []
+  const ESM_CDN = "https://esm.sh/";
+  const UNPKG_CDN = "https://unpkg.com/";
   const src = new URL(document.currentScript.src)
   for (const [key] of src.searchParams) {
     libraries.push(key)
   }
+  const mode = document.currentScript.getAttribute('data-mode') ?? 'javascript'
+  await initialize(libraries, mode);
 
-  await initialize(libraries);
+  function createScript(scriptName) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = `${UNPKG_CDN}${scriptName}`;
+      script.onload = function() {
+        return resolve()
+      }
+      script.onerror = function() {
+        return reject("Error occurred while loading script");
+      }
+      document.body.appendChild(script)
+    })
+  }
+
+  function createImportMap(libraries) {
+    const importMaps = {}
+    for (let imp of libraries) {
+      importMaps[imp.split('@')[0]] = `${ESM_CDN}${imp}`
+    }
+    const importMap = {
+      imports: importMaps,
+    };
+    const im = document.createElement("script");
+    im.type = "importmap";
+    im.textContent = JSON.stringify(importMap);
+    document.currentScript.after(im);
+  }
+
+  async function initialize(scriptNames = [], mode = 'javascript') {
+    const list = new Set([...scriptNames])
+    const eventReady = new CustomEvent('static-ready')
+
+    if (mode === "module") {
+      createImportMap(list)
+    } else {
+      try {
+        for(const scriptName of list) {
+          await createScript(scriptName)
+        }
+        document.dispatchEvent(eventReady);
+      } catch(err) {
+        console.error(err)
+      }
+    }
+  }
 })()
