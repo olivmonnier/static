4367ad92c197266d34d23dd7c99bd5e9c01dfb75
Olivier Monnier <omonnier.dev@gmail.com>
2023-02-23 22:20:45 +0100

    WIP

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 (async () => {
-  let libraries = []
+  let libraries = [];
   const ESM_CDN = "https://esm.sh/";
   const UNPKG_CDN = "https://unpkg.com/";
-  const src = new URL(document.currentScript.src)
+  const src = new URL(document.currentScript.src);
   for (const [key] of src.searchParams) {
-    libraries.push(key)
+    libraries.push(key);
   }
-  const mode = document.currentScript.getAttribute('data-mode') ?? 'javascript'
+  const mode = document.currentScript.getAttribute("data-mode") ?? "javascript";
   await initialize(libraries, mode);
 
+  /**
+   * Create Script element with the script name declared
+   * @param {string} scriptName - library name
+   */
   function createScript(scriptName) {
     return new Promise((resolve, reject) => {
-      const script = document.createElement('script');
+      const script = document.createElement("script");
       script.src = `${UNPKG_CDN}${scriptName}`;
-      script.onload = function() {
-        return resolve()
-      }
-      script.onerror = function() {
+      script.onload = function () {
+        return resolve();
+      };
+      script.onerror = function () {
         return reject("Error occurred while loading script");
-      }
-      document.body.appendChild(script)
-    })
+      };
+      document.body.appendChild(script);
+    });
   }
-
-  function createImportMap(libraries) {
-    const importMaps = {}
-    for (let imp of libraries) {
-      importMaps[imp.split('@')[0]] = `${ESM_CDN}${imp}`
+  /**
+   * Create script element as importmap type
+   * @param {string[]} scriptNames - library name list
+   */
+  function createImportMap(scriptNames) {
+    const importMaps = {};
+    for (let imp of scriptNames) {
+      importMaps[imp.split("@")[0]] = `${ESM_CDN}${imp}`;
     }
     const importMap = {
       imports: importMaps,
@@ -36,21 +43,25 @@
     im.textContent = JSON.stringify(importMap);
     document.currentScript.after(im);
   }
-
-  async function initialize(scriptNames = [], mode = 'javascript') {
-    const list = new Set([...scriptNames])
-    const eventReady = new CustomEvent('static-ready')
+  /**
+   * Initialize the library imports by mode type
+   * @param {string[]} scriptNames - library name list
+   * @param {"javascript" | "module"} mode - import type mode
+   */
+  async function initialize(scriptNames = [], mode = "javascript") {
+    const list = new Set([...scriptNames]);
+    const eventReady = new CustomEvent("static-ready");
 
     if (mode === "module") {
-      createImportMap(list)
+      createImportMap(list);
     } else {
       try {
-        for(const scriptName of list) {
-          await createScript(scriptName)
+        for (const scriptName of list) {
+          await createScript(scriptName);
         }
         document.dispatchEvent(eventReady);
-      } catch(err) {
-        console.error(err)
+      } catch (err) {
+        console.error(err);
       }
     }
   }
