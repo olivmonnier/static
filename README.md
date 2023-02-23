cb86eef8f9aad96c2ce2b198e62b7479584b90a4
Olivier Monnier <monnier@indig-net.com>
2023-02-23 14:21:54 +0100

    WIP

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-## Vue3 [url](https://cdn.statically.io/gh/olivmonnier/static/master/vue3/index.js)
-### Dependencies
-- router (vue-router)
-- i18n (vue-i18n)
+## Examples
+### Vue3
+
+```html
+<div id="app">{{ message }}</div>
+<script src="https://cdn.statically.io/gh/olivmonnier/static/master/index.js?vue@3&vue-router@4&vue-i18n@8"></script>
+<script type="module">
+  document.addEventListener('static-ready', () => {
+    const { createApp } = Vue
+    createApp({
+      data() {
+        return {
+          message: 'Hello Vue!'
+        }
+      }
+    }).mount('#app')
+  })
+</script>
+```
---
+++ index.js
@@ -0,0 +1,54 @@
+const ESM_CDN = "https://esm.sh";
+const CJS_CDN = "https://unpkg.com";
+
+function createScript(scriptName) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = `${CJS_CDN}/${scriptName}`
+    script.onload = function() {
+      return resolve()
+    }
+    script.onerror = function() {
+      return reject("Error occurred while loading script");
+    }
+    document.body.appendChild(script)
+  })
+}
+
+function createImportMap(libraries) {
+  const importMaps = {}
+  for (let imp of libraries) {
+    importMaps[imp.split('@')[0]] = `${ESM_CDN}/${imp}}`
+  }
+  const importMap = {
+    imports: importMaps,
+  };
+  const im = document.createElement("script");
+  im.type = "importmap";
+  im.textContent = JSON.stringify(importMap);
+  document.currentScript.after(im);
+}
+
+async function initialize(scriptNames = []) {
+  const list = new Set([...scriptNames])
+  const eventReady = new CustomEvent('static-ready')
+  createImportMap(list)
+  try {
+    for(const scriptName of list) {
+      await createScript(scriptName)
+    }
+    document.dispatchEvent(eventReady);
+  } catch(err) {
+    console.error(err)
+  }
+}
+
+(async () => {
+  let libraries = []
+  const src = new URL(document.currentScript.src)
+  for (const [key] of src.searchParams) {
+    libraries.push(key)
+  }
+
+  await initialize(libraries);
+})()
