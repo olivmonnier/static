759b42432933e81ce2a10247b570cdc044326e1c
Olivier Monnier <monnier@indig-net.com>
2023-02-24 17:09:01 +0100

    Add cdn attribute

--- index.js
+++ index.js
@@ -7,16 +7,19 @@
     libraries.push(key);
   }
   const mode = document.currentScript.getAttribute("data-mode") ?? "javascript";
-  await initialize(libraries, mode);
+  const cdn = document.currentScript.getAttribute("data-cdn");
+  await initialize(libraries, mode, cdn);
 
   /**
    * Create Script element with the script name declared
    * @param {string} scriptName - library name
+   * @param {string} cdn - cdn address
    */
-  function createScript(scriptName) {
+  function createScript(scriptName, cdn) {
     return new Promise((resolve, reject) => {
+      const host = cdn ?? UNPKG_CDN;
       const script = document.createElement("script");
-      script.src = `${UNPKG_CDN}${scriptName}`;
+      script.src = `${host}${scriptName}`;
       script.onload = function () {
         return resolve();
       };
@@ -29,11 +32,13 @@
   /**
    * Create script element as importmap type
    * @param {string[]} scriptNames - library name list
+   * @param {string} cdn - cdn address
    */
-  function createImportMap(scriptNames) {
+  function createImportMap(scriptNames, cdn) {
     const importMaps = {};
+    const host = cdn ?? ESM_CDN;
     for (let imp of scriptNames) {
-      importMaps[imp.split("@")[0]] = `${ESM_CDN}${imp}`;
+      importMaps[imp.split("@")[0]] = `${host}${imp}`;
     }
     const importMap = {
       imports: importMaps,
@@ -47,17 +52,23 @@
    * Initialize the library imports by mode type
    * @param {string[]} scriptNames - library name list
    * @param {"javascript" | "module"} mode - import type mode
+   * @param {string} cdn - change cdn address
    */
-  async function initialize(scriptNames = [], mode = "javascript") {
+  async function initialize(scriptNames = [], mode = "javascript", cdn = null) {
+    let host = cdn
     const list = new Set([...scriptNames]);
     const eventReady = new CustomEvent("static-ready");
 
+    if (typeof host === "string") {
+      host = host.slice(-1) !== "/" ? host + "/" : host
+    }
+
     if (mode === "module") {
-      createImportMap(list);
+      createImportMap(list, host);
     } else {
       try {
         for (const scriptName of list) {
-          await createScript(scriptName);
+          await createScript(scriptName, host);
         }
         document.dispatchEvent(eventReady);
       } catch (err) {
